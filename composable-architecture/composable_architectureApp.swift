d3062ccdc2dd354ca1abd35ece52ffa4c331b5e5
= <arya.cia@tokopedia.com>
2021-04-21 13:59:16 +0700

    Initial Commit

---
+++ composable-architecture/composable_architectureApp.swift
@@ -0,0 +1,17 @@
+//
+//  composable_architectureApp.swift
+//  composable-architecture
+//
+//  Created by arya.surya on 21/04/21.
+//
+
+import SwiftUI
+
+@main
+struct composable_architectureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
